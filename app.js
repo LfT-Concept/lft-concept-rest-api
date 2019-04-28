d369b94382177d8defc30af9179988b987a5151d
Syed Ahmad <syed-ahmad@outlook.com>
2019-04-28 07:33:09 +0100

    Add home page response

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ const app = express();
 
 app.use('/feed', feedRoutes);
 
+app.use('/', (req, res, next) => {
+  res.status(200).send("<h1>Lft Concept REST API</h1>").end();
+});
+
 app.listen(process.env.PORT || 8080);
