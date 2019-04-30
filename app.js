fccc213f72a9a81276e0e0928f9e7ec9224fb3a4
Syed Ahmad <syed-ahmad@outlook.com>
2019-04-30 09:04:43 +0100

    Add CORS headers

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ const app = express();
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded </form>
 app.use(bodyParser.json()); // Content-Type: application/json
 
+// CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.use('/', (req, res, next) => {
