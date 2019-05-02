479aa973167e0c30df3b87cd102c9f3c2c7b13e4
Syed Ahmad <syed-ahmad@outlook.com>
2019-05-02 13:00:36 +0100

    Remove method FETCH

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json()); // Content-Type: application/json
 // CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE, FETCH');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
