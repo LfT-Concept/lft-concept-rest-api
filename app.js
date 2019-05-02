ffafb2919d2b7106c2df651b26eb3081e88561ad
Syed Ahmad <syed-ahmad@outlook.com>
2019-05-02 12:56:34 +0100

    Enable home page response

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-// app.use('/', (req, res, next) => {
-//   res.status(200).send("<h1>Lft Concept REST API</h1>").end();
-// });
+app.use('/', (req, res, next) => {
+  res.status(200).send("<h1>Lft Concept REST API</h1>").end();
+});
 
 app.listen(process.env.PORT || 8080);
