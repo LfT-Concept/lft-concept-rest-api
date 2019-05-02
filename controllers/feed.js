a804836c5cf5ee4d87e232142d4e5d9bc8418650
Syed Ahmad <syed-ahmad@outlook.com>
2019-05-02 13:08:52 +0100

    Fix createPost return data

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,8 +1,8 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [{
-      _id: '1', 
-      title: 'First Post', 
+      _id: '1',
+      title: 'First Post',
       content: 'This is the first post!',
       imageUrl: 'images/duck.jpg',
       creator: {
@@ -20,7 +20,13 @@ exports.createPost = (req, res, next) => {
   // Create a post in db
   res.status(201).json({
     message: 'Post created successfully',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    post: { 
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: 'Syed'},
+      createdAt: new Date()
+    }
   });
 
 };
