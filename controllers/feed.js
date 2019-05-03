98be8c3d98d8047efeae7ed4ec561c19be11f8f3
Syed Ahmad <syed-ahmad@outlook.com>
2019-05-03 10:40:45 +0100

    Add server side validation using express validator pacakage

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator/check');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [{
@@ -14,17 +16,26 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: 'Validation failed, entered data is incorrect',
+        errors: errors.array()
+      })
+  }
   const title = req.body.title;
   const content = req.body.content;
   console.log(req.body);
   // Create a post in db
   res.status(201).json({
     message: 'Post created successfully',
-    post: { 
+    post: {
       _id: new Date().toISOString(),
       title: title,
       content: content,
-      creator: { name: 'Syed'},
+      creator: { name: 'Syed' },
       createdAt: new Date()
     }
   });
