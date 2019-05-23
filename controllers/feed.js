d7b1a20d304a8ae0f0fdf7936edaec7f9159b688
Syed Ahmad <syed-ahmad@outlook.com>
2019-05-23 09:25:12 +0100

    Async await - fix /update feed controllers

--- controllers/feed.js
+++ controllers/feed.js
@@ -6,10 +6,9 @@ const Post = require('../models/posts');
 const User = require('../models/user');
 
 exports.getPosts = async (req, res, next) => {
-  const currentPage = req.query.page || 1;
-  const perPage = 2;
-  let totalItems;
   try {
+    const currentPage = req.query.page || 1;
+    const perPage = 2;
     const postCount = Post.find().countDocuments();
     const filteredPosts = Post.find().skip((currentPage - 1) * perPage).limit(perPage);
     // Parallel processing
@@ -29,32 +28,32 @@ exports.getPosts = async (req, res, next) => {
 };
 
 exports.createPost = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error('Validation failed, entered data is incorrect');
-    error.statusCode = 422;
-    throw error; // to be handled by express error handling middleware
-  }
-
-  if (!req.file) {
-    const error = new Error('No image provided');
-    error.statusCode = 422;
-    throw error;
-  }
-
-  const imageUrl = req.file.path;
-  const title = req.body.title;
-  const content = req.body.content;
-
-  const post = new Post({
-    title: title,
-    content: content,
-    imageUrl: imageUrl,
-    creator: req.userId // added by auth middleware to req
-  });
-
   try {
-    post.save(); // think no need to wait on this request but what happens if it errors / fails / takes too long? Rollback? 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error('Validation failed, entered data is incorrect');
+      error.statusCode = 422;
+      throw error; // to be handled by express error handling middleware
+    }
+
+    if (!req.file) {
+      const error = new Error('No image provided');
+      error.statusCode = 422;
+      throw error;
+    }
+
+    const imageUrl = req.file.path;
+    const title = req.body.title;
+    const content = req.body.content;
+
+    const post = new Post({
+      title: title,
+      content: content,
+      imageUrl: imageUrl,
+      creator: req.userId // added by auth middleware to req
+    });
+    //post.save(); // think no need to wait on this request but what happens if it errors / fails / takes too long? Rollback?
+    await post.save(); // if you do not await, promise reject won't throw
     const user = await User.findById(req.userId);
     user.posts.push(post);
 
@@ -73,9 +72,8 @@ exports.createPost = async (req, res, next) => {
 };
 
 exports.getPost = async (req, res, next) => {
-  const postId = req.params.postId;
-
   try {
+    const postId = req.params.postId;
     const post = await Post.findById(postId);
     if (!post) {
       const error = new Error(`Could not find post by id ${postId}`);
@@ -93,46 +91,49 @@ exports.getPost = async (req, res, next) => {
 };
 
 exports.updatePost = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error('Validation failed, entered data is incorrect');
-    error.statusCode = 422;
-    throw error;
-  }
-
-  const postId = req.params.postId;
-  const title = req.body.title;
-  const content = req.body.content;
-  let imageUrl = req.body.image;
-  if (req.file) {
-    // a new file uploaded on edit
-    imageUrl = req.file.path;
-  }
-  if (!imageUrl) {
-    const error = new Error('No file picked.');
-    error.statusCode = 422;
-    throw error;
-  }
-
   try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error('Validation failed, entered data is incorrect');
+      error.statusCode = 422;
+      throw error;
+    }
+
+    const postId = req.params.postId;
+    const title = req.body.title;
+    const content = req.body.content;
+    let imageUrl = req.body.image;
+    if (req.file) {
+      // a new file uploaded on edit
+      imageUrl = req.file.path;
+    }
+    if (!imageUrl) {
+      const error = new Error('No file picked.');
+      error.statusCode = 422;
+      throw error;
+    }
+
     const post = await Post.findById(postId);
     if (!post) {
       const error = new Error(`Could not find post by id ${postId}`);
       error.statusCode = 404;
       throw error;
     }
-    if (imageUrl !== post.imageUrl) {
-      clearImage(post.imageUrl);
-    }
-
+    
     post.title = title;
     post.imageUrl = imageUrl;
     post.content = content;
-
+    
     if (post.creator.toString() !== req.userId) {
       const error = new Error('Unauthorised to update this post');
       error.statusCode = 403;
       throw error;
+    }
+    
+    if (imageUrl !== post.imageUrl) {
+      // this could be a long process or can fail
+      // make it async and await it
+      clearImage(post.imageUrl);
     }
 
     const result = await post.save();
@@ -145,40 +146,36 @@ exports.updatePost = async (req, res, next) => {
   }
 };
 
-exports.deletePost = (req, res, next) => {
-  const postId = req.params.postId;
-  Post.findById(postId)
-    .then(post => {
-      if (!post) {
-        const error = new Error(`Post not found ${postId}`);
-        error.statusCode = 404;
-        throw error;
-      }
-      if (post.creator !== req.userId) {
-        const error = new Error('Unauthorised to delete this item');
-        error.statusCode = 403;
-        throw error;
-      }
-      clearImage(post.imageUrl);
-      return Post.findByIdAndRemove(postId)
-    })
-    .then(result => {
-      return User.findById(req.userId);
-    })
-    .then(user => {
-      user.posts.pull(postId);
-      return user.save();
-    })
-    .then(result => {
-      const message = `Post successfully deleted ${postId}`;
-      res.status(200).json({ message: message });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+exports.deletePost = async (req, res, next) => {
+  try {
+    const postId = req.params.postId;
+    const post = await Post.findById(postId);
+    if (!post) {
+      const error = new Error(`Post not found ${postId}`);
+      error.statusCode = 404;
+      throw error;
+    }
+    console.log(post.creator, post);
+    if (post.creator.toString() !== req.userId) {
+      const error = new Error('Unauthorised to delete this item');
+      error.statusCode = 403;
+      throw error;
+    }
+
+    clearImage(post.imageUrl);
+
+    await Post.findByIdAndRemove(postId);
+    const user = await User.findById(req.userId);
+    user.posts.pull(postId);
+    await user.save();
+    const message = `Post successfully deleted ${postId}`;
+    res.status(200).json({ message: message });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 const clearImage = (filePath) => {
